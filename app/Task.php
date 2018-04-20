3d9a6ebb83ad15fb6a9359ef813aa3711aafd96f
sireBlack <dblacktin@gmail.com>
2018-04-20 06:53:59 +0100

    initial commit

---
+++ app/Task.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Validation\Validator;
+
+class Task extends Model
+{
+    protected $fillable = [
+ 
+        'name',
+  
+        'user_id',
+  
+        'description',
+  
+    ];
+}
